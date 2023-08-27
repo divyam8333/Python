fe8dbc4097ae74244eb30acf8a2b21ab7075a359
Aman Chaturvedi <90045734+divyam8333@users.noreply.github.com>
2023-08-27 12:19:11 +0530

    Delete Assignment2 directory

--- Assignment2/ass1.py
+++
@@ -1,7 +0,0 @@
-num=int(input("Enter any Number:"))
-
-if num%2==0:
-    print(num,"is Even Number")
-else:
-    print(num,"is Odd Number")
-    
--- Assignment2/ass2.py
+++
@@ -1,6 +0,0 @@
-num=int(input("Enter any Number:"))
-
-if num%5==0:
-    print(num,"is Divisible by 5")
-else:
-    print(num,"is not Divisible by 5")
