86dc376600fe1739485259cd5e40f6c92725c39d
Aman Chaturvedi <90045734+divyam8333@users.noreply.github.com>
2023-08-27 12:19:28 +0530

    Delete Assignment4 directory

--- Assignment4/ass7.py
+++
@@ -1,5 +0,0 @@
-n=int(input("Enter any number: "))
-
-print("First",n,"Even Natural numbers in reverse order are ",end=" ")
-for i in range(n*2,1,-2):
-  print(i,end=" ")
--- Assignment4/ass9.py
+++
@@ -1,9 +0,0 @@
-print("Enter 2 Numbers:")
-a=int(input())
-b=int(input())
-
-for i in range(1,a*b):
-    if i%a==0 and i%b==0:
-        break
-
-print("L.C.M of",a,"and",b,"is",i)
