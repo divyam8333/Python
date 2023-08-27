7e95f8140ca587d0fc6a6b1f3847ac08b0eda165
Aman Chaturvedi <90045734+divyam8333@users.noreply.github.com>
2023-08-27 12:15:46 +0530

    Delete Assignment9 directory

--- Assignment9/ass1.py
+++
@@ -1,5 +0,0 @@
-x = {2, 3, 5, 6}
-y = {1, 2, 3, 4}
-
-z = x.intersection(y)
-print(z)
--- Assignment9/ass7.py
+++
@@ -1,7 +0,0 @@
-s1={12,34,22,54,34,5}
-s2={54,5}
-
-#issubset()
-print(s2.issubset(s1))
-#issuperset()
-print(s1.issuperset(s2))
